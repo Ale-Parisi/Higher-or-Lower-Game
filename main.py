01bb4d0bf9b7c602ec73e3524dcc47cff53dde67
Ale-Parisi <121767224+Ale-Parisi@users.noreply.github.com>
2023-02-08 20:18:14 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+import art
+import game_data
+import random
+from replit import clear
+
+print(art.logo)
+end_game = 0
+
+score = 0
+#print(element_a)
+
+while end_game == 0:
+    if score == 0:
+        element_a = random.choice(game_data.data)
+    element_b = random.choice(game_data.data)
+    if element_a == element_b:
+        element_b = random.choice(game_data.data)    
+    print(f"Compare A: {element_a['name']}, a {element_a['description']}, from {element_a['country']}.")
+    print(art.vs)
+    print(f"Against B: {element_b['name']}, a {element_b['description']}, from {element_b['country']}.")
+    selection = input("Who has more Instagram followers? Type A or B: ")
+    if element_a['follower_count'] > element_b['follower_count'] and selection == "A":
+        score += 1
+        clear()
+        print(f"You're right! Current score: {score}")
+        element_a = element_b
+        
+    elif element_b['follower_count'] > element_a['follower_count'] and selection == "B":
+        score += 1
+        clear()
+        print(f"You're right! Current score: {score}")
+        element_a = element_b
+        
+    else:
+        print(f"Sorry, that's wrong. Final score {score}")
+        end_game = 1
+    
